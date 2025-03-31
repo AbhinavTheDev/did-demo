be6760edba6b792ce6f3096b1d2daf9f805b54bb
Abhinav <85792055+AbhinavTheDev@users.noreply.github.com>
2025-03-31 07:58:07 +0530

    url

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // Configuration for DID Auth
 const CONFIG = {
     // DID Auth Server URL
-    AUTH_SERVER: 'http://localhost:3000',
+    AUTH_SERVER: 'https://did-hero.vercel.app/',
     
     // Client ID and secret obtained
     CLIENT_ID: '', 
     CLIENT_SECRET: '', 
     
     // Redirect URI
-    REDIRECT_URI: 'http://localhost:5500/callback.html'
+    REDIRECT_URI: 'https://did-demo-weld.vercel.app/callback.html'
 };
